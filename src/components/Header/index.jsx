39869eeb47b20391603d69f4e1f83f068eee78fa
Gabriel Ricardo <gabrielricardo7@gmail.com>
2022-06-29 11:32:00 -0300

    fix: change package.json and rename App to Book Shop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,15 @@ const Header = () => {
   return (
     <>
       <MyHeader>
-        <img src="images/book-pile-by-gabrielricardo7(256).png" alt="Books" />
-        <h1>KenzieShop<br /><small> by Gabriel Ricardo</small></h1>
+        <img
+          src="images/book-pile-by-gabrielricardo7(256).png"
+          alt="Books"
+        />
+        <h1>
+          Book Shop
+          <br />
+          <small> by Gabriel Ricardo</small>
+        </h1>
         <Cart />
       </MyHeader>
     </>
